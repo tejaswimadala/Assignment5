bc703a51ecd51394287d23fa67b47d47df3f9ab6
tejaswimadala <139881898+tejaswimadala@users.noreply.github.com>
2024-09-21 15:47:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+console.log("Welcome to My Website!");
+document.getElementById('registration-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Registration form submitted!');
+});
+
+document.getElementById('login-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Login form submitted!');
+});
+
+
+
+
+
+// Simulate data retrieval
+console.log("Dashboard data loaded.");
+
+
+
+console.log("About Us page loaded.");
